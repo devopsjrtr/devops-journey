d7b2e7dcc50855e06c32323bd989c582cf7e3a99
devopsjrtr <osman.saygili@yandex.com>
2026-08-07 09:37:27 +0300

    Test automated webhook pipeline trigger

--- day-10/app.py
+++ day-10/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>DevOps Journey - Day 10: Continous Deployment Success! 🚀</h1>"
+    return "<h1> Hello Osman! Automatic Webhook Deployment Works Perfectly! 🚀</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
